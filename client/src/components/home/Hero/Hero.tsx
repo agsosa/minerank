014e328ac83e4402ed4a22a0671697d428ec5787
Alejandro Sosa <agsosa74@gmail.com>
2022-02-06 00:56:23 -0300

    Update CTA btn

--- client/src/components/home/Hero/Hero.tsx
+++ client/src/components/home/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import { Container, ContentOverlay, HeroContent, Left, Right } from "./Hero.styled";
 import Image from "next/image";
 import { Button } from "@mui/material";
+import { FaSearch } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -14,7 +15,9 @@ const Hero = () => {
           <h1>Descubre los mejores servidores de Minecraft</h1>
           <h2>La lista de servidores de Minecraft #1</h2>
           <h3>Más de 150 servidores de Minecraft, 50 categorías, 150 jugadores en línea y más!</h3>
-          <Button variant="contained" size="large">Explorar Servidores</Button>
+          <Button startIcon={<FaSearch />} variant="contained" size="large">
+            Explorar Servidores
+          </Button>
         </Right>
       </HeroContent>
     </Container>
