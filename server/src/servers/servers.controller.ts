2636548ce720a63b825afd429dba410462957f26
Alejandro Sosa <agsosa74@gmail.com>
2022-02-15 13:09:56 -0300

    Add api versioning

--- server/src/servers/servers.controller.ts
+++ server/src/servers/servers.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ServersService } from './servers.service';
 import { CreateServerDto } from './dto/create-server.dto';
 import { UpdateServerDto } from './dto/update-server.dto';
 
-@Controller('servers')
+@Controller({
+  version: '1',
+  path: 'servers',
+})
 export class ServersController {
   constructor(private readonly serversService: ServersService) {}
 
