78fd797b2878ee177c1416269e31ddf08fb01744
Alejandro Sosa <agsosa74@gmail.com>
2022-02-15 13:26:36 -0300

    Update server entity

--- server/src/servers/entities/server.entity.ts
+++ server/src/servers/entities/server.entity.ts
@@ -1,16 +1,80 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Server {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  // Server details
+  @Column({
+    length: 36,
+  })
   name: string;
+
+  @Column({
+    length: 280,
+  })
+  description: string;
+
+  @Column({
+    length: 24,
+  })
+  path: string;
 
   @Column()
   ip: string;
 
+  @Column()
+  version: string;
+
+  @Column()
+  premiumType: string;
+
+  @Column()
+  countryCode: string;
+
+  // Server status
   @Column({ default: true })
   isActive: boolean;
+
+  @Column()
+  upvotes: number;
+
+  // Social media
+  @Column()
+  website?: string;
+
+  @Column()
+  youtube?: string;
+
+  @Column()
+  twitter?: string;
+
+  @Column()
+  discord?: string;
+
+  @Column()
+  telegram?: string;
+
+  @Column()
+  facebook?: string;
+
+  @Column()
+  instagram?: string;
+
+  @Column()
+  teamspeak?: string;
+
+  // Timestamps
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
