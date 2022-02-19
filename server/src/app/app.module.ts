d17fb252879c625428e6a41bb31b1729454ff2b2
Alejandro Sosa <agsosa74@gmail.com>
2022-02-19 01:23:40 -0300

    Update app.module.ts

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigModuleOptions, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { CommunityModule } from 'src/community/community.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const configModuleOptions: ConfigModuleOptions = {
+  isGlobal: true,
+  cache: true,
+};
+
+const typeOrmModuleAsyncOptions: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService) => ({
+    type: configService.get<any>('DB_TYPE'),
+    host: configService.get<string>('DB_HOST'),
+    port: parseInt(configService.get<string>('DB_PORT')),
+    username: configService.get<string>('DB_USER'),
+    password: configService.get<string>('DB_PASS'),
+    database: configService.get<string>('DB_NAME'),
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),
+  inject: [ConfigService],
+};
+
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      cache: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<any>('DB_TYPE'),
-        host: configService.get<string>('DB_HOST'),
-        port: parseInt(configService.get<string>('DB_PORT')),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASS'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
+    ConfigModule.forRoot(configModuleOptions),
+    TypeOrmModule.forRootAsync(typeOrmModuleAsyncOptions),
     CommunityModule,
   ],
   controllers: [AppController],
