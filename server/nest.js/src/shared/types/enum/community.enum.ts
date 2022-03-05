5fbff7807630c6269adcbb50cefa17286b9b6dd0
Alejandro Sosa <agsosa74@gmail.com>
2022-03-05 14:11:20 -0300

    Update community.enum.ts

--- server/nest.js/src/shared/types/enum/community.enum.ts
+++ server/nest.js/src/shared/types/enum/community.enum.ts
@@ -1,6 +1,6 @@
 export enum PremiumTypeEnum {
-  NO_PREMIUM = "NO_PREMIUM",
-  SEMI_PREMIUM = "SEMI_PREMIUM",
+  NO_PREMIUM = "NO-PREMIUM",
+  SEMI_PREMIUM = "SEMI-PREMIUM",
   PREMIUM = "PREMIUM",
 }
 
