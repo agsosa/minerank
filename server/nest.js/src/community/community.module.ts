3e718eddde0a88a3d3b6cc774690292495fb3994
Alejandro Sosa <agsosa74@gmail.com>
2022-03-18 13:27:24 -0300

    Fix server startup error

--- server/nest.js/src/community/community.module.ts
+++ server/nest.js/src/community/community.module.ts
@@ -4,9 +4,10 @@ import { CommunityController } from './community.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Community } from './community.entity';
 import { GameMode } from 'src/gamemode/gamemode.entity';
+import { Version } from 'src/version/version.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Community, GameMode])],
+  imports: [TypeOrmModule.forFeature([Community, GameMode, Version])],
   controllers: [CommunityController],
   providers: [CommunityService],
 })
